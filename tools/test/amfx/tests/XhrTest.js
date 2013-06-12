64721003727b671bcf4a3c68e47171f3245f057a
emilkm <emil@localhost>
2013-06-12 21:12:06 +1200

    XhrTest.js update

--- tools/test/amfx/tests/XhrTest.js
+++ tools/test/amfx/tests/XhrTest.js
@@ -3,7 +3,7 @@ enyo.kind({
 	kind: enyo.TestSuite,
 	testXhrSync: function() {
 		var x = enyo.xhr.request({
-			url: "http://127.0.0.1:8080/lib/amfx/tools/test/amfx/php/test1.php?format=text",
+			url: "php/test1.php?format=text",
 			sync: true
 		});
 		if (x.responseText) {
@@ -13,22 +13,22 @@ enyo.kind({
 			this.finish("sync XHR didn't return with text");
 		}
 	},
-    testXhrArrayBuffer: function() {
-        var self = this;
-        enyo.xhr.request({
-            url: "http://127.0.0.1:8080/lib/amfx/tools/test/amfx/php/test8.php",
-            method: "POST",
-            sync: false,
-            body: new Uint8Array([]),
-            headers: {"Content-Type": "application/x-amf; charset=UTF-8"},
-            xhrFields: {responseType: "arraybuffer"},
-            callback: function (inText, inXhr) {
-                if (inXhr.response instanceof ArrayBuffer) {
-                    self.finish("");
-                } else {
-                    self.finish("Response not an arraybuffer");
-                }
-            }
-        });
-    }
+	testXhrArrayBuffer: function() {
+		var self = this;
+		enyo.xhr.request({
+			url: "php/test8.php",
+			method: "POST",
+			sync: false,
+			body: new Uint8Array([]),
+			headers: {"Content-Type": "application/x-amf; charset=UTF-8"},
+			xhrFields: {responseType: "arraybuffer"},
+			callback: function (inText, inXhr) {
+				if (inXhr.response instanceof ArrayBuffer) {
+					self.finish("");
+				} else {
+					self.finish("response is not an ArrayBuffer");
+				}
+			}
+		});
+	}
 });
